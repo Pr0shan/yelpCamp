c0e40a440a183e4fd8a17d1f093cd5a33e53af13
Prashant Pandey <0902prashant@gmail.com>
2023-01-28 15:28:56 +0530

    added the reviews refernce and deletion middleware

--- models/campground.js
+++ models/campground.js
@@ -1,12 +1,29 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema;
+const Review = require('./review')
+const {Schema} = mongoose
 
 const CampGroundSchema = new Schema({
     title: String,
     image:String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:'Review'
+        }
+    ]
+})
+
+CampGroundSchema.post('findOneAndDelete', async function(campground){
+    if(campground){
+        await Review.deleteMany({
+            _id:{
+                $in:campground.reviews
+            }
+        })
+    }
 })
 
 module.exports = mongoose.model('Campground',CampGroundSchema)
